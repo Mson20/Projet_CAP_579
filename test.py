acb0fbc5c79bff93fbaa252f1fa2754ec42bacdd
Daitori <100677113+Daitori@users.noreply.github.com>
2023-01-24 17:09:07 +0100

    Ajout fonction + chg risk
    Fonction read_file_to_db et read db

--- test.py
+++ test.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import numpy as np
 import sqlite3 as sl
-limit_max=1000 
-#1000 iteration SSID selectionné arbitrairement, test à réaliser pour déterminer la valeur optimale
-Data=pd.read_table(r"CapProjet\test.txt",header=None,sep=" ").to_numpy()  #Change le chemin d'accès du fichier
-date="2020-05-01" #Date de la capture de données
-con=sl.connect(r"CapProjet\test.db") #Change le chemin d'accès de la base de données
-#date de la capture de données, normalement on modifie le .sh pour récupérer la date du jour
-#pour avoir une suivie sur plusieurs jours, on peut créer une table par jour
 
-X=Data[:,-1]
-SSID=np.unique(X)
-df_SSID=pd.DataFrame({'SSID' : [],'Number_SSID':[],'Risk':[]})
-for x in SSID:
-    Number_SSID=(np.count_nonzero(X==x))
-    if x in df_SSID.SSID: #On vérifie si le SSID est déjà présent dans la table et on met à jour les valeurs normalement c'est pas possible car on a un SSID unique par ligne mais on sait jamais
-        #amélioration possible .update()
-        df_SSID.loc[df_SSID.SSID==x,'Number_SSID']=Number_SSID
-        df_SSID.loc[df_SSID.SSID==x,'Risk']=Number_SSID/limit_max
-    else: #On ajoute le SSID à la table
+def read_file_to_db(text_file,db_file,date):
+    limit_max=1000 
+    #1000 iteration SSID selectionné arbitrairement, test à réaliser pour déterminer la valeur optimale
+    Data=pd.read_table(text_file,header=None,sep=" ").to_numpy()
+    con=sl.connect(db_file)
+    X=Data[:,-1] #
+    SSID=np.unique(X)
+    df_SSID=pd.DataFrame({'SSID' : [],'Number_SSID':[],'Risk':[]})
+    for x in SSID:
+        Number_SSID=(np.count_nonzero(X==x))
         d1=df_SSID
-        d2=pd.DataFrame({'SSID':x,'Number_SSID':Number_SSID,'Risk':Number_SSID/limit_max},index=[0])
+        d2=pd.DataFrame({'SSID':x,'Number_SSID':Number_SSID,'Risk':(Number_SSID/limit_max)*100},index=[0])
+        #Risk, un pourchentage(0% à 100%)
         df_SSID=pd.concat([d1,d2])
+    df_SSID.to_sql(date,con,if_exists='replace',index=False)
+    #date de la capture de données, normalement on modifie le .sh pour récupérer la date du jour
+    #pour avoir une suivie sur plusieurs jours, on peut créer une table par jour
+    #date=table
 
-df_SSID.to_sql(date,con,if_exists='replace',index=False)
+def read_db(db_file,date):
+    con=sl.connect(db_file)
+    df = pd.read_sql_query("SELECT * from '"+date+"'", con)
+    con.close()
+    return df
+
+filename="test.txt"
+db_name="test.db"
+date="2023-01-24"
+read_file_to_db(filename,db_name,date)
+df=read_db(db_name,date)
+print(df)
