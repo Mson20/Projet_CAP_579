30488af0eaefc2c9db4bb105b6de7167cea42e73
Daitori <100677113+Daitori@users.noreply.github.com>
2023-03-12 23:40:43 +0100

    Update OnLaunch.sh

--- OnLaunch.sh
+++ OnLaunch.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-DATE=$(date +%d-%m-%Y).txt
 while true; do
-exec IFACE=wlan0 OUTPUT=DATE CHANNEL_HOP=1 ./sniff-probes.sh
+exec IFACE=wlan0 OUTPUT=OUT.txt CHANNEL_HOP=1 ./sniff-probes.sh
 python todb.py
 done
